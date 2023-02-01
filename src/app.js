cb0d4df990754e941bbb99e740f3acff060943d2
Html-No0b <rmartin9797@gmail.com>
2023-01-31 22:47:46 -0600

    adding tailwind

--- src/app.js
+++ src/app.js
@@ -1,16 +1,18 @@
 import React from "react";
-import Footer from "./components/Footer"
-import Header from "./components/Header"
-import Navigation from "./components/Navigation"
-import Project from "./components/Project"
-export default function App(){
-    return (
-        <main>
-            <navbar></navbar>
-            <Header />
-            <Project />
-            <Footer />
-            <Navigation />
-        </main>
-    )
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Navigation from "./components/Navigation";
+import Project from "./components/Project";
+
+
+export default function App() {
+  return (
+    <main>
+      <navbar></navbar>
+      <Header />
+      <Project />
+      <Footer />
+      <Navigation />
+    </main>
+  );
 }
